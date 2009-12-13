161b4eb3443b2a3eecd01f803e7ab101f57630c9
tim <tim3d.junk@gmail.com>
2009-12-13 21:54:59 +1100

    more appropriate email address

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	version='0.1.0',
 	description='a dataflow library for python',
 	author='Tim Cuthbertson',
-	author_email='tim3d.junk+mocktest@gmail.com',
+	author_email='tim3d.junk+dataflow@gmail.com',
 	url='http://github.com/gfxmonk/py-dataflow/tree',
 	packages=find_packages(exclude=["test"]),
 	
